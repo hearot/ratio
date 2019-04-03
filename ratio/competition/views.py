5bf78bf89c118c1b2e987f625175e22700e433b3
Hearot <gabriel@hearot.it>
2019-04-03 19:29:44 +0200

    competitions: add custom error messages

--- ratio/competition/views.py
+++ ratio/competition/views.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import EmptyPage, Paginator
 from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
 from django.views import generic
 
 from .models import Competition, Contestant, User
@@ -30,15 +31,21 @@ def join_competition(request, pk: int = 1):
     try:
         competition = Competition.objects.get(id=pk)
     except ObjectDoesNotExist:
-        return render(request, "layouts/default/page.html", {'error': True})
+        return render(request, "layouts/default/page.html", {'error': True,
+                                                             'error_message':
+                                                                 _("The competition you want to join doesn't exist!")})
 
     if not request.user.is_authenticated:
-        return render(request, "layouts/default/page.html", {'error': True})
+        return render(request, "layouts/default/page.html", {'error': True,
+                                                             'error_message':
+                                                                 _("You must be logged in to join a competition!")})
 
     user = User.objects.get(username=request.user.username)
 
     if Contestant.objects.filter(competition=competition, user=user).exists():
-        return render(request, "layouts/default/page.html", {'error': True})
+        return render(request, "layouts/default/page.html", {'error': True,
+                                                             'error_message':
+                                                                 _("You've already joined this competition!")})
 
     contestant = Contestant(competition=competition, user=user)
     contestant.save()
